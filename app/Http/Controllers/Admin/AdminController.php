421122327d6e01e5d8e629238a4b2795ddf7ca64
obadaajenah <95519889+obadaajenah@users.noreply.github.com>
2023-08-12 13:06:53 +0300

    modifay admin and update

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,11 +3,59 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Models\Partner;
+use App\Models\User;
 use App\Http\Controllers\Controller;
+use App\Models\RequestsPartner;
 
 class AdminController extends Controller
 {
-    public function setGemsValue(){}
+    public function setGemsValue(Request $request){
+
+
+    }
+
+
     public function setBonusValue(){}
+
+
+
+    public function modfiy(Request $request ,$id ){
+
+
+
+    $set= RequestsPartner::find($id);
+     if($set->stutes=='0' || $set->states=='nul'){
+
+      if($request->status =='accept'){
+
+      $re= RequestsPartner::find($id);
+
+       $user=user::find($re->user_id);
+       $partner=partner::create([
+        'user_id'=>$user->id,
+     ]);
+     $user->update(['role_id'=>2]);
+     $TT=RequestsPartner::find($id);
+     $TT->update(['status' => 1]);
+     return response()->json(['message'=>"Gongrats we add new partner"]);
+
+       } elseif($request->status =='reject'){
+
+        $TT=RequestsPartner::find($id);
+        $TT->update(['status' => 0]);
+
+        return response()->json(['message'=>"we did it",]);
+       }
+
+    }
+       return response()->json(['message'=>'we decided before']);
+
+
+
+
+    }
+
+
 
 }
